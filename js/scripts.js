b959bc05c802cb84d2845567efe825ac2df4b430
Grace Stuart and Sara Schultz <grace@>
2017-02-16 14:14:14 -0800

    declare variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  $("#entry-form").submit(function(event) {
+  var side1 = parseInt("#side1").val();
+  var side2 = parseInt("#side2").val();
+  var side3 = parseInt("#side3").val();
 
   event.preventDefault();
+  });
 });
