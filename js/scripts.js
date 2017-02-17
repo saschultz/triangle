4f0fd6b885248e155624e5cd59c93b62558c1f14
Grace Stuart and Sara Schultz <grace@>
2017-02-16 16:34:36 -0800

    correct multiple errors in bot files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,36 @@
 $(document).ready(function() {
+
   $("#entry-form").submit(function(event) {
-  var side1 = parseInt("#side1").val();
-  var side2 = parseInt("#side2").val();
-  var side3 = parseInt("#side3").val();
+    debugger;
+  var side1 = parseInt($("#side1").val());
+  var side2 = parseInt($("#side2").val());
+  var side3 = parseInt($("#side3").val());
 
   if (side1 && side2 && side3) {
-    } else if (side1 + side2 >= side3) {
-      alert("The side lengths entered are not a triangle!");
+    // all fields in the form have a value which is a number
+    if (side1 + side2 >= side3) {
+        alert("1 - The side lengths entered are not a triangle!");
+      }
+      else if (side1 + side3 >= side2) {
+        alert("2 - The side lengths entered are not a triangle!");
+      }
+      else if (side2 + side3 >= side1) {
+        alert("3 - The side lengths entered are not a triangle!");
+      }
+      else if (side1 === side2 && side2 === side3) {
+        alert("This is an equilateral triangle!");
+      }
+      else if (side1 === side2 || side1 === side3 || side2 === side3) {
+        alert("This is an isosceles triangle!");
+      }
+    else {
+      alert("This is a scalene triangle!");
     }
-    else if (side1 + side3 >= side2) {
-      alert("The side lengths entered are not a triangle!");
-    }
-    else if (side2 + side3 >= side1) {
-      alert("The side lengths entered are not a triangle!");
-    }
-    else if (side1 === side2 === side3) {
-      alert("This is an equilateral triangle!")
-    }
-    else if (side1 === side2 || side1 === side3 || side2 === side3) {
-      alert("This is an isosceles triangle!")
-    }
-    else { 
-      console.log("something is broken");
-      alert("This is a scalene triangle!")
+  }
+  else {
+    //prompt user to enter numbers
+    alert("Please enter a number in each field.");
+    console.log("something is broken");
     }
 
   event.preventDefault();
